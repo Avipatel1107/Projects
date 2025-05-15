edd2d03c9947e86b080cbb771cac5a494c544f39
Avi Patel <90337579+Avipatel1107@users.noreply.github.com>
2025-05-15 14:49:08 -0500

    import and export

---
+++ React-Projects/simplejs/assets/scripts/app.js
@@ -0,0 +1,6 @@
+
+//import apiKey from "./util.js";
+
+import * as utils from "./util.js";
+
+console.log(utils.apiKey);
---
+++ React-Projects/simplejs/assets/scripts/util.js
@@ -0,0 +1,5 @@
+//export let apiKey = "aksdjafaskfjasfkj";
+
+export default "aksdjafaskfjasfkj";
+export let apiKey = "aksdjafaskfjasfkj";
+export let abc = "abc";
