d3f26c9487d225d05f7d2e9225bcd498cc033bc0
Avi Patel <90337579+Avipatel1107@users.noreply.github.com>
2025-05-15 15:17:27 -0500

    JS Finished git comm

--- React-Projects/simplejs/assets/scripts/app.js
+++ React-Projects/simplejs/assets/scripts/app.js
@@ -1,6 +1,12 @@
 
 //import apiKey from "./util.js";
 
-import * as utils from "./util.js";
+//import * as utils from "./util.js";
 
-console.log(utils.apiKey);
+//console.log(utils.apiKey);
+
+function greet(){
+    console.log("Hello!")
+}
+
+greet();
