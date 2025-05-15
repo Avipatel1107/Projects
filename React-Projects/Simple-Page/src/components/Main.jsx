7cb94706b35b6c7ffe48fb3ae9784d7983dd3d72
Avi Patel <90337579+Avipatel1107@users.noreply.github.com>
2025-05-15 18:46:34 +0000

    Simple page

---
+++ React-Projects/Simple-Page/src/components/Main.jsx
@@ -0,0 +1,16 @@
+export default function Main(){
+    return(
+        
+        <main>
+            <h1 className="header-main">Fun facts about React</h1>
+                <ul className="ul-main">
+                    <li>Was first released in 2013</li>
+                    <li>Was originally created by Jordan Walke</li>
+                    <li>Has wll over 200k stars on GitHub</li>
+                    <li>Is maintained by Meta</li>
+                    <li>Powers thousands of enterprise apps, including mobile apps</li>
+                </ul>
+        </main>
+        
+    )
+}
---
+++ React-Projects/Simple-Page/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+
+export default function Navbar(){
+    return(
+        <header className="header">
+            <nav className="navbar"> 
+                <img src="logo192.png" className="logo" alt="React Logo"/>
+                <span>ReactFacts</span>
+            </nav>
+        </header>
+    )
+}
